2703fb7193e437c3694d073284d49b24e2e3766e
twinzies <kjaintara@gmail.com>
2025-09-23 19:43:27 +0100

    feat: Add modified pipeline run and intermediary steps.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,31 @@
+"""
+DeepReflect: A Pipeline for Analyzing and generating responses to personal queries.
+===========================================
+
+This module implements a pipeline to analyze and generate responses to personal queries using LLMs.
+
+Author: Tara K. Jain
+Copyright (c) 2025. All rights reserved.
+Version: 1.0.0
+License: Proprietary
+"""
+
 import argparse
 import logging
 from pathlib import Path
+import pandas as pd
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 data_dir = Path("../data/raw_reddit")
+secret_outputs = Path("../data/outputs") # Untracked folder for full outputs with PII.
 output_dir = Path("../data/dr-outputs")
 
 class DeepReflectPipeline:
     """Pipeline to analyze and compare Reddit human-authored responses and LLM generated ones to personal queries."""
 
     def __init__(self, data_dir: Path, output_dir: Path, cot_mode: bool = False, num_posts: int = 1, subreddit: str = None) -> None:
+
         self.data_dir = data_dir
         self.output_dir = output_dir
         self.subreddit = subreddit # If None, dataset not expected to be RedArcs.
@@ -18,8 +33,47 @@ class DeepReflectPipeline:
         self.num_posts = num_posts
         self.cot_mode = cot_mode
 
-    def run(self, cot_mode: bool) -> None:
+    def extract_posts(data_dir = data_dir) -> None:
         pass
+
+    def generate_llm_response(self, cot_mode: bool) -> None:
+        if cot_mode:
+            # The cot_mode subsystem uses chain-of-thought prompting.
+            pass
+        else:
+            pass
+
+    def load_paradigms(self) -> None:
+        pass
+
+    def annotate_responses(self) -> None:
+        pass
+
+    def save_full_outputs(self) -> None:
+        # Save the full outputs to the untracked folder.
+        pass
+
+    def save_outputs(self) -> None:
+        pass
+
+    def run(self, cot_mode: bool) -> None:
+        logger.info("Beginning DeepReflectPipeline...")
+
+        logger.info("Extracting posts...")
+        self.extract_posts()
+
+        logger.info("Generating LLM responses...")
+        self.generate_llm_response(cot_mode)
+
+        logger.info("Loading paradigms...")
+        self.load_paradigms()
+
+        logger.info("Annotating responses...")
+        self.annotate_responses()
+
+        logger.info("Saving annotated outputs...")
+        self.save_outputs()
+        return True
 
 def main():
     # Read in csv file as per the argument.
